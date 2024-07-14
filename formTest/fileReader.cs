3c085cff10f0a8323e509dc4696da232b5d2a251
Taufeeq Ali <taufeeq_26@outlook.com>
2024-07-14 11:12:16 -0500

    This is a push to see if fileReader will be added to the branch

---
+++ formTest/fileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWindowsForms
+{
+    internal class fileReader
+    {
+        private StreamReader reader;
+        public String testPath;
+        private string filePath;
+
+        public fileReader(string path)
+        {
+            this.filePath = path;
+
+            if (File.Exists(filePath)) 
+            {
+                testPath = "File Path Exists";
+            } else
+            {
+                testPath = "File Path does not exist";
+            }
+        }
+    }
+       
+}
